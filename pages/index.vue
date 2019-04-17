bf795fc578441f59f8e53a82bb6cb373e3edf668
Mitsuya.WATANABE <watanabe@gocco.co.jp>
2019-04-17 18:30:52 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <h1>Nuxt template( h1 color has been already set by _base.scss)</h1>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.container {}
+</style>
